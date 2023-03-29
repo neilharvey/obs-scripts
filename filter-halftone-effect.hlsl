a82cc2840209f7e4d943bb3ac1c9bd513326c770
Neil Harvey <neil.harvey@hotmail.co.uk>
2023-03-29 23:15:38 +0100

    Add half-tone filter

---
+++ filter-halftone-effect.hlsl
@@ -0,0 +1,92 @@
+// OBS-specific syntax adaptation to HLSL standard to avoid errors reported by the code editor
+#define SamplerState sampler_state
+#define Texture2D texture2d
+
+// Constants
+#define PI 3.141592653589793238
+
+// Uniform variables set by OBS (required)
+uniform float4x4 ViewProj; // View-projection matrix used in the vertex shader
+uniform Texture2D image;   // Texture containing the source picture
+
+// General properties
+uniform float gamma = 1.0;
+uniform float gamma_shift = 0.6;
+uniform float amplitude = 0.2;
+uniform float scale = 1.0;
+uniform int number_of_color_levels = 4.0;
+
+// Size of the source picture
+uniform int width;
+uniform int height;
+
+// General properties
+
+// Interpolation method and wrap mode for sampling a texture
+SamplerState linear_clamp
+{
+    Filter    = Linear;     // Anisotropy / Point / Linear
+    AddressU  = Clamp;      // Wrap / Clamp / Mirror / Border / MirrorOnce
+    AddressV  = Clamp;      // Wrap / Clamp / Mirror / Border / MirrorOnce
+    BorderColor = 00000000; // Used only with Border edges (optional)
+};
+
+// Data type of the input of the vertex shader
+struct vertex_data
+{
+    float4 pos : POSITION;  // Homogeneous space coordinates XYZW
+    float2 uv  : TEXCOORD0; // UV coordinates in the source picture
+};
+
+// Data type of the output returned by the vertex shader, and used as input 
+// for the pixel shader after interpolation for each pixel
+struct pixel_data
+{
+    float4 pos : POSITION;  // Homogeneous screen coordinates XYZW
+    float2 uv  : TEXCOORD0; // UV coordinates in the source picture
+};
+
+// Vertex shader used to compute position of rendered pixels and pass UV
+pixel_data vertex_shader_halftone(vertex_data vertex)
+{
+    pixel_data pixel;
+    pixel.pos = mul(float4(vertex.pos.xyz, 1.0), ViewProj);
+    pixel.uv  = vertex.uv;
+    return pixel;
+}
+
+float3 decode_gamma(float3 color, float exponent, float shift)
+{
+    return pow(clamp(color, 0.0, 1.0), exponent - shift);
+}
+
+float3 encode_gamma(float3 color, float exponent)
+{
+    return pow(clamp(color, 0.0, 1.0), 1.0/exponent);
+}
+
+
+// Pixel shader used to compute an RGBA color at a given pixel position
+float4 pixel_shader_halftone(pixel_data pixel) : TARGET
+{
+    float4 source_sample = image.Sample(linear_clamp, pixel.uv);
+    float3 linear_color = decode_gamma(source_sample.rgb, gamma, gamma_shift);
+
+    float luminance = dot(linear_color, float3(0.299, 0.587, 0.114));
+    float2 position = pixel.uv * float2(width, height);
+    float perturbation = amplitude * cos(PI*position.x/scale/4.0) * cos(PI*position.y/scale/4.0);
+    float3 result = (luminance + perturbation).xxx;
+
+    result = round((number_of_color_levels-1)*result)/(number_of_color_levels-1);
+
+    return float4(encode_gamma(result, gamma), source_sample.a);
+}
+
+technique Draw
+{
+    pass
+    {
+        vertex_shader = vertex_shader_halftone(vertex);
+        pixel_shader  = pixel_shader_halftone(pixel);
+    }
+}
---
+++ filter-halftone.lua
@@ -0,0 +1,134 @@
+-- Source: https://obsproject.com/wiki/Scripting-Tutorial-Halftone-Filter
+obs = obslua
+
+-- Returns the description displayed in the Scripts window
+function script_description()
+    return [[Halftone Filter
+    This Lua script adds a video filter named Halftone.     
+    The filter can be added to a video source to reduce the number of colors of the input picture. 
+    It reproduces the style of a magnified printed picture.]]
+end
+
+-- Called on script startup
+function script_load(settings)
+    obs.obs_register_source(source_info)
+end
+
+source_info = {}
+source_info.id = 'filter-halftone' -- Unique string identifier of the source type
+source_info.type = obs.OBS_SOURCE_TYPE_FILTER -- INPUT or FILTER or TRANSITION
+source_info.output_flags = obs.OBS_SOURCE_VIDEO -- Combination of VIDEO/AUDI/ASYNC/etc
+
+-- Returns the name displayed in the list of filters
+source_info.get_name = function()
+    return "Halftone"
+end
+
+-- Sets the default settings for this source
+source_info.get_defaults = function(settings)
+    obs.obs_data_set_default_double(settings, "gamma", 1.0)
+    obs.obs_data_set_default_double(settings, "gamma_shift", 0.0)
+    obs.obs_data_set_default_double(settings, "scale", 1.0)
+    obs.obs_data_set_default_double(settings, "amplitude", 0.2)
+    obs.obs_data_set_default_int(settings, "number_of_color_levels", 4)
+end
+
+-- Gets the property information of this source
+source_info.get_properties = function(data)
+    local props = obs.obs_properties_create()
+    obs.obs_properties_add_float_slider(props, "gamma", "Gamma encoding exponent", 1.0, 2.2, 0.2)
+    obs.obs_properties_add_float_slider(props, "gamma_shift", "Gamma shift", -2.0, 2.0, 0.01)
+    obs.obs_properties_add_float_slider(props, "scale", "Pattern scale", 0.01, 10.0, 0.01)
+    obs.obs_properties_add_float_slider(props, "amplitude", "Perturbation amplitude", 0.0, 2.0, 0.01)
+    obs.obs_properties_add_int_slider(props, "number_of_color_levels", "Number of color levels", 2, 10, 1)
+
+    return props
+end
+
+-- Updates the internal data for this source upon settings change
+source_info.update = function(data, settings)
+    data.gamma = obs.obs_data_get_double(settings, "gamma")
+    data.gamma_shift = obs.obs_data_get_double(settings, "gamma_shift")
+    data.scale = obs.obs_data_get_double(settings, "scale")
+    data.amplitude = obs.obs_data_get_double(settings, "amplitude")
+    data.number_of_color_levels = obs.obs_data_get_int(settings, "number_of_color_levels")
+end
+
+-- Creates the implementation data for the source
+source_info.create = function(settings, source)
+
+    -- Initializes the custom data table
+    local data = {}
+    data.source = source -- Keeps a reference to this filter as a source object
+    data.width = 1 -- Dummy value during initialization phase
+    data.height = 1 -- Dummy value during initialization phase
+
+    -- Compiles the effect
+    obs.obs_enter_graphics()
+    local effect_file_path = script_path() .. 'filter-halftone-effect.hlsl'
+    data.effect = obs.gs_effect_create_from_file(effect_file_path, nil)
+    obs.obs_leave_graphics()
+
+    -- Calls the destroy function if the effect was not compiled properly
+    if data.effect == nil then
+        obs.blog(obs.LOG_ERROR, "Effect compilation failed for " .. effect_file_path)
+        source_info.destroy(data)
+        return nil
+    end
+
+    -- Retrieves the shader uniform variables
+    data.params = {}
+    data.params.width = obs.gs_effect_get_param_by_name(data.effect, "width")
+    data.params.height = obs.gs_effect_get_param_by_name(data.effect, "height")
+    data.params.gamma = obs.gs_effect_get_param_by_name(data.effect, "gamma")
+    data.params.gamma_shift = obs.gs_effect_get_param_by_name(data.effect, "gamma_shift")
+    data.params.amplitude = obs.gs_effect_get_param_by_name(data.effect, "amplitude")
+    data.params.scale = obs.gs_effect_get_param_by_name(data.effect, "scale")
+    data.params.number_of_color_levels = obs.gs_effect_get_param_by_name(data.effect, "number_of_color_levels")
+
+    -- Calls update to initialize the rest of the properties-managed settings
+    source_info.update(data, settings)
+
+    return data
+end
+
+-- Returns the width of the source
+source_info.get_width = function(data)
+    return data.width
+end
+
+-- Returns the height of the source
+source_info.get_height = function(data)
+    return data.height
+end
+
+-- Called when rendering the source with the graphics subsystem
+source_info.video_render = function(data)
+    local parent = obs.obs_filter_get_parent(data.source)
+    data.width = obs.obs_source_get_base_width(parent)
+    data.height = obs.obs_source_get_base_height(parent)
+
+    obs.obs_source_process_filter_begin(data.source, obs.GS_RGBA, obs.OBS_NO_DIRECT_RENDERING)
+
+    -- Effect parameters initialization goes here
+    obs.gs_effect_set_int(data.params.width, data.width)
+    obs.gs_effect_set_int(data.params.height, data.height)
+    obs.gs_effect_set_float(data.params.gamma, data.gamma)
+    obs.gs_effect_set_float(data.params.gamma_shift, data.gamma_shift)
+    obs.gs_effect_set_float(data.params.amplitude, data.amplitude)
+    obs.gs_effect_set_float(data.params.scale, data.scale)
+    obs.gs_effect_set_int(data.params.number_of_color_levels, data.number_of_color_levels)
+
+    obs.obs_source_process_filter_end(data.source, data.effect, data.width, data.height)
+
+end
+
+-- Destroys and release resource linked to the custom data
+source_info.destroy = function(data)
+    if data.effect ~= nil then
+        obs.obs_enter_graphics()
+        obs.gs_effect_destroy(data.effect)
+        data.effect = nil
+        obs.obs_leave_graphics()
+    end
+end
